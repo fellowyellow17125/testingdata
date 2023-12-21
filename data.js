5b30a0211403e4e76f621483b33914effa456907
fellowyellow17125 <133915690+fellowyellow17125@users.noreply.github.com>
2023-12-21 04:20:09 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+var chartData = [
+    { region: 'Midwest', value: 36.3 },
+    { region: 'Northeast', value: 34.7 },
+    { region: 'West', value: 12.9 },
+    { region: 'South', value: 8.6 },
+    { region: 'Europe', value: 3.3 },
+    { region: 'Asia', value: 3.3 },
+    { region: 'Africa', value: 0.3 },
+    { region: 'Americas', value: 0.3 },
+    { region: 'Middle East', value: 0.1 },
+    { region: 'Australia', value: 0.1 }
+];
